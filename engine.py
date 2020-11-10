a15869ae998ec12f1b19145462341e7e357b7698
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-10 15:39:38 -0500

    accidently put a ',' instead of a ':'

--- engine.py
+++ engine.py
@@ -27,7 +27,7 @@ class Engine:
       elif isinstance(action, EscapeAction):
         raise SystemExit()
 
-  def render(self, console: Console, context, Context) -> None:
+  def render(self, console: Console, context: Context) -> None:
     for entity in self.entities:
       console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
