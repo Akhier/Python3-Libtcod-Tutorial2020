6093dc8ee87d2db4b5743218f1a98e66db03ddf8
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-11 17:38:10 -0500

    add a call to an unwritten function

--- procgen.py
+++ procgen.py
@@ -94,6 +94,8 @@ def generate_dungeon(
       for x, y in tunnel_between(rooms[-1].center, new_room.center):
         dungeon.tiles[x, y] = tile_types.floor
 
+    place_entities(new_room, dungeon, max_monsters_per_room)
+
     rooms.append(new_room)
 
   return dungeon
