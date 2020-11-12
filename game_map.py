9c23998d0b6d5337aa252bc6ce350e693b0aab32
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-12 13:42:23 -0500

    modify game map to use actors

--- game_map.py
+++ game_map.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 
 import numpy as np
 from tcod.console import Console
 
+from entity import Actor
 import tile_types
 
 if TYPE_CHECKING:
@@ -28,6 +29,14 @@ class GameMap:
       (width, height), fill_value=False, order="F"
     )
 
+  @property
+  def actors(self) -> Iterator[Actor]:
+    yield from (
+      entity
+      for entity in self.entities
+      if isinstance(entity, Actor) and entity.is_alive
+    )
+
   def get_blocking_entity_at_location(
     self, location_x: int, location_y: int
   ) -> Optional[Entity]:
@@ -40,6 +49,13 @@ class GameMap:
         return entity
 
     return None
+
+  def get_actor_at_location(self, x: int, y: int) -> Optional[Actor]:
+    for actor in self.actors:
+      if actor.x == x and actor.y == y:
+        return actor
+
+    return
 
   def in_bounds(self, x: int, y: int) -> bool:
     return 0 <= x < self.width and 0 <= y < self.height
