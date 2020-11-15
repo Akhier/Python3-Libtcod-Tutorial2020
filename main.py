782be28a8e14a967192a1d213942ed909109093e
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-15 18:48:26 -0500

    modify main to take into account the changes to event handler

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import tcod
 import color
 from engine import Engine
 import entity_factories
+import exceptions
+import input_handlers
 from procgen import generate_dungeon
 
 def main() -> None:
@@ -46,6 +48,8 @@ def main() -> None:
     "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
   )
 
+  handler: input_handlers.BaseEventHandler = input_handlers.MainGameEventHandler(engine)
+
   with tcod.context.new_terminal(
     screen_width,
     screen_height,
@@ -54,18 +58,30 @@ def main() -> None:
     vsync=True
   ) as context:
     root_console = tcod.Console(screen_width, screen_height, order="F")
-    while True:
-      root_console.clear()
-      engine.event_handler.on_render(console=root_console)
-      context.present(root_console)
+    try:
+      while True:
+        root_console.clear()
+        handler.on_render(console=root_console)
+        context.present(root_console)
 
-      try:
-        for event in tcod.event.wait():
-          context.convert_event(event)
-          engine.event_handler.handle_events(event)
-      except Exception: # Handle exceptions in game
-        traceback.print_exc() # Print error to stderr
-        engine.message_log.add_message(traceback.format_exc(), color.error)
+        try:
+          for event in tcod.event.wait():
+            context.convert_event(event)
+            handler = handler.handle_events(event)
+        except Exception:
+          traceback.print_exc()
+          if isinstance(handler, input_handlers.EventHandler):
+            handler.engine.message_log.add_message(
+              traceback.format_exc(), color.error
+            )
+    except exceptions.QuitWithoutSaving:
+      raise
+    except SystemExit:
+      # TODO: Add the save function here
+      raise
+    except BaseException:
+      # TODO: add the save function here
+      raise
 
 if __name__ == "__main__":
   main()
