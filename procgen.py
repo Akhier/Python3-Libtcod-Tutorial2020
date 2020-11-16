8668e5d49ef6cad01d3b9a81dc6e198c0985e586
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-16 16:00:05 -0500

    fixed another transcription error

--- procgen.py
+++ procgen.py
@@ -113,10 +113,10 @@ class RectangularRoom:
 
 def place_entities(room: RectangularRoom, dungeon: GameMap, floor_number: int) -> None:
   number_of_monsters = random.randint(
-      0, get_weight_for_floor(max_monsters_by_floor, floor_number)
+      0, get_max_value_for_floor(max_monsters_by_floor, floor_number)
   )
   number_of_items = random.randint(
-      0, get_weight_for_floor(max_items_by_floor, floor_number)
+      0, get_max_value_for_floor(max_items_by_floor, floor_number)
   )
 
   monsters: List[Entity] = get_entities_at_random(
