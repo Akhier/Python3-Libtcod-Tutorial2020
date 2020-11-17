389b6ce3fb698f8359dcc3fa550ed9b2a9757f64
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-11-17 02:17:15 -0500

    modify input handlers to take into account equipment

--- input_handlers.py
+++ input_handlers.py
@@ -320,7 +320,15 @@ class InventoryEventHandler(AskUserEventHandler):
     if number_of_items_in_inventory > 0:
       for i, item in enumerate(self.engine.player.inventory.items):
         item_key = chr(ord("a") + i)
-        console.print(x + 1, y + i + 1, f"({item_key}) {item.name}")
+
+        is_equipped = self.engine.player.equipment.item_is_equipped(item)
+
+        item_string = f"({item_key}) {item.name}"
+
+        if is_equipped:
+          item_string = f"{item_string} (E)"
+
+        console.print(x + 1, y + i + 1, item_string)
     else:
       console.print(x + 1, y + 1, "(Empty)")
 
@@ -339,7 +347,12 @@ class InventoryEventHandler(AskUserEventHandler):
     return super().ev_keydown(event)
 
   def on_item_selected(self, item: Item) -> Optional[ActionOrHandler]:
-    raise NotImplementedError()
+    if item.consumable:
+      return item.consumable.get_action(self.engine.player)
+    elif item.equippable:
+      return actions.EquipAction(self.engine.player, item)
+    else:
+      return None
 
 
 class InventoryActivateHandler(InventoryEventHandler):
